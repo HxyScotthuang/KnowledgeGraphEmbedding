e65cbb934384eb42f8813234e6b59dd61220d97f
Xingyue Huang <49787534+HxyScotthuang@users.noreply.github.com>
2023-03-02 01:56:34 +0000

    Create download_data.sh

---
+++ download_data.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget 'https://www.dropbox.com/scl/fo/69p37v7285vx6whxf6hrt/h?dl=1&rlkey=94vpdnvmwx0bco5e1pjwrdtzi'
+mkdir data
+unzip 'h?dl=1&rlkey=94vpdnvmwx0bco5e1pjwrdtzi' -d data
+rm 'h?dl=1&rlkey=94vpdnvmwx0bco5e1pjwrdtzi'
