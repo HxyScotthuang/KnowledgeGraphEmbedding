6a74ed017a2a2f505f88dea847e844d5afe7b5c7
Xingyue Huang <orie4090@htc-login01.arcus-htc.arc.local>
2023-03-02 02:50:32 +0000

    write some runnable file

---
+++ rotate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --time=11:59:59
+#SBATCH --job-name=kge
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=short
+#SBATCH --gres=gpu:v100:1
+
+module purge
+module load Anaconda3
+source activate $DATA/myenv
+
+
+CUDA_VISIBLE_DEVICES=0 python3.9 -u codes/run.py --do_train \
+ --cuda \
+ --do_test \
+ --data_path data/Hetionet_inv \
+ --model RotatE \
+ -n 256 -b 256 -d 512 \
+ -g 24.0 -a 1.0 -adv \
+ -lr 0.0001 --max_steps 150000 \
+ -save models/RotatE_Het_0 --test_batch_size 16 -de
